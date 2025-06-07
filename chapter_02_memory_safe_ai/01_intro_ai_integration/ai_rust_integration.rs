34f19ffb4c11cc6a735e23b5f22e4b54491670f2
Ashish Sharda <ashishjsharda@gmail.com>
2025-06-07 15:33:15 -0400

    Create ai_rust_integration.rs

---
+++ chapter_02_memory_safe_ai/01_intro_ai_integration/ai_rust_integration.rs
@@ -0,0 +1,13 @@
+use smartcore::prelude::*;
+
+// For neural networks
+use candle_core::{Device, Tensor};
+
+// For interfacing with existing Python models
+use pyo3::prelude::*;
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    // We can now integrate AI capabilities directly
+    // into our memory-safe Rust applications
+    Ok(())
+}
